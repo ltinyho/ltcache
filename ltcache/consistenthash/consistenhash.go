7d993765a96118a84eca4ac163e3ae8e9944a051
ltinyho <ltinyho@gmail.com>
2021-07-13 08:26:49 +0800

    add 一致性hash算法

---
+++ ltcache/consistenthash/consistenhash.go
@@ -0,0 +1,59 @@
+package consistenthash
+
+import (
+	"fmt"
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
+
+// Hash uint32 表示环的大小
+type Hash func(data []byte) uint32
+
+type Map struct {
+	hash     Hash
+	replicas int            // 虚拟节点倍数
+	keys     []int          // 哈希环
+	hashMap  map[int]string // 虚拟节点与真实节点的映射表,键是虚拟节点的哈希值,值是真实节点的名称
+}
+
+func New(replicas int, fn Hash) *Map {
+	m := &Map{
+		hash:     fn,
+		replicas: replicas,
+		keys:     nil,
+		hashMap:  make(map[int]string),
+	}
+	if m.hash == nil {
+		m.hash = crc32.ChecksumIEEE
+	}
+	return m
+}
+
+func (m *Map) Add(keys ...string) {
+	for _, key := range keys {
+		for i := 0; i < m.replicas; i++ {
+			// 转成int方便使用标准库里的函数
+			hash := int(m.hash([]byte(strconv.Itoa(i) + key)))
+			m.keys = append(m.keys, hash)
+			m.hashMap[hash] = key
+		}
+	}
+
+	sort.Ints(m.keys)
+	fmt.Println(m.keys)
+}
+
+func (m *Map) Get(key string) string {
+	if len(m.keys) == 0 {
+		return ""
+	}
+
+	hash := int(m.hash([]byte(key)))
+	idx := sort.Search(len(m.keys), func(i int) bool {
+		return m.keys[i] >= hash
+	})
+	nodeHash := m.keys[idx%len(m.keys)]
+	return m.hashMap[nodeHash]
+
+}
